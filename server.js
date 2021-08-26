10f199f7f9b0c2bcf701a33cfe9486c9874c66af
Kumar Adhikari / Avee <kumarad556@gmail.com>
2021-08-26 20:15:46 +0545

    room added

--- server.js
+++ server.js
@@ -10,6 +10,10 @@ const port = process.env.PORT || 5000;
 
 app.use(express.static(path.join(__dirname, "public")));
 
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public/index.html"));
+});
+
 app.get("/chat", (req, res) => {
   res.sendFile(path.join(__dirname, "public/chat.html"));
 });
